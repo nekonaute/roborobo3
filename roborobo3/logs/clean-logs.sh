1ee700cc9ed67a6bc80e125c8dcce0013162515a
nekonaute <nicolas.bredeche@upmc.fr>
2016-01-22 22:42:39 +0100

    Added ./logs/clean-logs.sh

---
+++ roborobo3/logs/clean-logs.sh
@@ -0,0 +1,8 @@
+rm -f logs/datalog_*.txt
+rm -f logs/properties_*.txt
+rm -f logs/screenshot_*.bmp
+rm -f logs/trajectory_*.bmp
+rm -f logs/robotsId*.bmp
+rm -f logs/*.eps
+rm -f logs/log.txt
+rm -f logs/extract*.dat*
