fcd1e6a22efb8e5dbd30f31234088555df218ae6
nekonaute <nicolas.bredeche@upmc.fr>
2017-01-25 18:04:07 +0100

    added replicate.py

---
+++ roborobo3/replicate.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import os
+import sys
+import datetime
+import time
+
+debug = False   # if True: shell command are not issued, but displayed.
+
+def createdir(directory):
+	if not os.path.exists(directory):
+        	if debug == False:
+	                os.makedirs(directory)
+	        print "[INFO] directory \"",directory,"\" created."
+	else:
+	        print "[ERROR] directory already exists."
+	        quit()
+
+if len(sys.argv) == 1:
+	print "Syntax: ", sys.argv[0], " <path/file.properties> <nb_of_replicates> {<directory_suffix>}"	
+	print "\texample: python replicate.py config/template_vanillaEE.properties 32"
+	quit()
+else:
+	propertiesfile = sys.argv[1]
+	nbrep = int(sys.argv[2])
+	if len(sys.argv) == 4:
+		suffix = sys.argv[3]
+	else:
+		suffix = "exp"
+
+prefix = "./logs/"+datetime.datetime.now().strftime("%Y%m%d_%H%M")
+
+for i in range(nbrep):
+	print "[INFO] Starting replicate #",i
+	
+	dirname = prefix + "_" + str(i).zfill(len(str(nbrep)))+ "_" + suffix 
+	
+	createdir(dirname)
+	
+	command = "nohup ./roborobo -l " + propertiesfile + " -o " + dirname + " 1> " + dirname + "/output.stdout 2> " + dirname + "/output.stderr &"
+	
+	if debug == False:
+		os.system(command)
+	else:
+		print command
+	
+	time.sleep(0.2)
+
+print "[INFO] Terminated."
