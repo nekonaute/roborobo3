e1a08ffdcbbbe23b3186ac2b2afe840242418352
nekonaute <nicolas.bredeche@upmc.fr>
2017-12-04 12:05:00 +0100

    minor modifs; moved clean-logs to tools directory; suggested: create symlink in logs

---
+++ roborobo3/tools/scripts/clean-logs.sh
@@ -0,0 +1,13 @@
+rm -f datalog_*.txt
+rm -f lite_*.txt
+rm -f graph*pdf
+rm -f properties_*.txt
+rm -f screenshot_*
+rm -f snapshot_*
+rm -f trajectory_*
+rm -f robotsId*
+rm -f *.eps
+rm -f log.txt
+rm -f extract*.dat*
+rm -f output.std*
+
